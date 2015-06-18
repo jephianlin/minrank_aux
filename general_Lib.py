f90362672bc49f7b3be0edf2fc1f33528652139d
jephianlin <jephianlin@gmail.com>
2015-06-18 02:26:26 -0500

    Update general_Lib.py

--- general_Lib.py
+++ general_Lib.py
@@ -1,4 +1,37 @@
+def empty_array(m,n):
+    """
+    Input: int m,n;
+    Output: the two-dimension array contains m copies of [0]*n;
+    """
+    a=[];
+    for i in range(m):
+        a.append([0]*n);
+    return a;
+    
+def all_one_matrix(m,n):
+    """
+    Input: int m,n;
+    Output: the all one matrix of size m x n;
+    """
+    return matrix(m,[1]*(m*n));  
+
+def elementary_matrix(i,j,m,n=None):
+    """
+    Input: int i,j,m,n; if n is not assigned, then n=m;
+    Output: an m x n elementary matrix whose i,j-entry is 1 while all others are 0;
+    ## starting from 0th column and 0th row;
+    """
+    if n==None:
+        n=m;
+    a=[0]*(m*n);
+    a[i*n+j]=1;
+    return matrix(m,a);
+
 def eigens_multi(A):
+    """
+    Input: matrix A;
+    Output: the dictionary of {eigenvalues: multiplicity};
+    """
     l=A.eigenvalues();
     eigens={};
     for i in l:
@@ -8,6 +41,10 @@ def eigens_multi(A):
     return eigens;
     
 def sort_dictionary(d):
+    """
+    Input: dict d;
+    Output: print key: value following the order of keys;
+    """
     l=d.keys();
     l.sort();
     #print l;

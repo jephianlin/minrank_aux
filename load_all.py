6bbda0a580ae3b4531abb1242d32f12fbded2be6
Jephian Lin <jephianlin@gmail.com>
2015-11-03 23:03:12 -0600

    Update load_all.py

--- load_all.py
+++ load_all.py
@@ -1,5 +1,5 @@
 URL='https://raw.githubusercontent.com/jephianlin/minimum_rank_aux/master/'
-files=['oc_diag_analysis.sage','xi_dict.py','mu_dict.py', 'general_Lib.py','SXP.sage']
+files=['general_Lib.py','oc_diag_analysis.sage','xi_dict.py','mu_dict.py','SXP.sage']
 for f in files:
     print "Loading %s..."%f;
     load(URL+f);

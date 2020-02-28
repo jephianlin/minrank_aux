3062064e1bf90694d4140b48ba413d8463d4c712
Jephian Lin <jephianlin@gmail.com>
2020-02-28 11:06:22 +0800

    Update load_all.py

--- load_all.py
+++ load_all.py
@@ -1,5 +1,27 @@
-URL='https://raw.githubusercontent.com/jephianlin/minrank_aux/master/'
-files=['general_Lib.sage','oc_diag_analysis.sage','xi_dict.py','mu_dict.py','SXP.sage','matrix_forcing.py']
-for f in files:
-    print("Loading %s..."%f);
-    load(URL+f);
+# increase timeout seconds
+def loadurl(url, timeout=5):
+    temp_name = tmp_filename() + '.' + os.path.splitext(url)[1][1:]
+
+    from urllib.request import urlopen
+    content = urlopen(url, timeout=timeout)
+    with open(temp_name, 'wb') as f:
+        f.write(content.read())
+    sage.repl.load.load(temp_name, globals())
+    os.unlink(temp_name)
+
+def load_all(mr_JG=True, minrank_aux=True, timeout=5, load_func='loadurl'):
+    func = eval(load_func) # load_func can be 'loadurl' (default) or 'load'
+    
+    if mr_JG:
+        URL='https://raw.githubusercontent.com/jephianlin/mr_JG/master/'
+        files=['Zq_c.pyx','Zq.py','zero_forcing_64.pyx','zero_forcing_wavefront.pyx','minrank.py', 'inertia.py']
+        for f in files:
+            print("Loading %s..."%f);
+            func(URL+f) 
+    
+    if minrank_aux:
+        URL='https://raw.githubusercontent.com/jephianlin/minrank_aux/master/'
+        files=['general_Lib.sage','oc_diag_analysis.sage','xi_dict.py','mu_dict.py','SXP.sage','matrix_forcing.py']
+        for f in files:
+            print("Loading %s..."%f);
+            func(URL+f);

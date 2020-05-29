66c0b6e829b1c0258489ce504371a3da66e5b1db
Jephian Lin <jephianlin@gmail.com>
2020-05-29 10:53:45 +0800

    load_all.py add local keyword

--- README.md
+++ README.md
@@ -4,7 +4,21 @@ auxiliary tools for the minimum rank problem
 
 Run the code below:
 
-```python
+```Python
 load("https://raw.githubusercontent.com/jephianlin/minrank_aux/master/load_all.py")
 load_all()
+```
+
+If you are using CoCalc or there is problem loading the libraries from the internet.  You may download the two repositories below by clicking **Clone or download** and **Download ZIP**.
+- [mr_JG](https://github.com/jephianlin/mr_JG)
+- [minrank_aux](https://github.com/jephianlin/minrank_aux)
+Upload the two zip files to CoCalc and put them in the folder where your Jupyter notebook (or *.sagews) is.  Unzip the files so that your folder looks like
+    my_project.ipynb
+    mr_JG-master/
+    minrank_aux-master/    
+Now run the code below in your Jupyter notebook.
+
+```Python
+load("minrank_aux-master/load_all.py")
+load_all(loacl=True)
 ```
--- load_all.py
+++ load_all.py
@@ -1,3 +1,4 @@
+
 # increase timeout seconds
 def loadurl(url, timeout=5):
     temp_name = tmp_filename() + '.' + os.path.splitext(url)[1][1:]
@@ -9,8 +10,9 @@ def loadurl(url, timeout=5):
     sage.repl.load.load(temp_name, globals())
     os.unlink(temp_name)
 
-def load_all(mr_JG=True, minrank_aux=True, timeout=5, load_func='load'):
+def load_all(mr_JG=True, minrank_aux=True, timeout=5, load_func='load', local=False):
     # load_func can be 'loadurl' (default) or 'load'
+    # local can be False, True, or 'your_path_to_libraries' (e.g., '~/Download/')
     if load_func == 'load':
         func = load
     if load_func == 'loadurl':
@@ -22,18 +24,29 @@ def load_all(mr_JG=True, minrank_aux=True, timeout=5, load_func='load'):
         ### temporary setting
         ### hope the version become more consistent in the future
         if sage_ver >= 8.9:
-            URL='https://raw.githubusercontent.com/jephianlin/mr_JG/master/'
+            URL = 'https://raw.githubusercontent.com/jephianlin/mr_JG/master/'
         else:
-            URL='https://raw.githubusercontent.com/jephianlin/mr_JG/py2/'
+            URL = 'https://raw.githubusercontent.com/jephianlin/mr_JG/py2/'
+
+        if local == True: 
+            URL = 'mr_JG-master/'
+        elif isinstance(local,str):
+            URL = local + 'mr_JG-master/'
             
-        files=['Zq_c.pyx','Zq.py','zero_forcing_64.pyx','zero_forcing_wavefront.pyx','minrank.py', 'inertia.py']
+        files = ['Zq_c.pyx','Zq.py','zero_forcing_64.pyx','zero_forcing_wavefront.pyx','minrank.py', 'inertia.py']
         for f in files:
             print("Loading %s..."%f);
             func(URL+f) 
     
     if minrank_aux:
-        URL='https://raw.githubusercontent.com/jephianlin/minrank_aux/master/'
-        files=['general_Lib.sage','oc_diag_analysis.sage','xi_dict.py','mu_dict.py','SXP.sage','matrix_forcing.py']
+        URL = 'https://raw.githubusercontent.com/jephianlin/minrank_aux/master/'
+
+        if local == True: 
+            URL = 'minrank_aux-master/'
+        elif isinstance(local,str):
+            URL = local + 'minrank_aux-master/'
+
+        files = ['general_Lib.sage','oc_diag_analysis.sage','xi_dict.py','mu_dict.py','SXP.sage','matrix_forcing.py']
         for f in files:
             print("Loading %s..."%f);
             func(URL+f);
